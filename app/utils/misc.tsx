e62ec37f25368249d3004810d8bf4f2501c1d0d8
Zane Hitchcox <zwhitchcox@gmail.com>
2024-08-20 20:26:25 -0400

    ts-expect-error

--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -335,18 +335,18 @@ export function gtmPush(...args: any[]) {
 
 export function addGTM() {
 	;(function (w, d, s, l, i) {
-		// @ts-ignore
+		// @ts-expect-error
 		w[l] = w[l] || []
-		// @ts-ignore
+		// @ts-expect-error
 		w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
 		var f = d.getElementsByTagName(s)[0],
 			j = d.createElement(s),
 			dl = l != 'dataLayer' ? '&l=' + l : ''
-		// @ts-ignore
+		// @ts-expect-error
 		j.async = true
-		// @ts-ignore
+		// @ts-expect-error
 		j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-		// @ts-ignore
+		// @ts-expect-error
 		f.parentNode.insertBefore(j, f)
 	})(window, document, 'script', 'dataLayer', ENV.GTM_ID)
 }
