916013da423a72ade50dfee3e775ecbda37579b8
Zane Hitchcox <zwhitchcox@gmail.com>
2025-04-03 13:25:31 -0400

    update every 10 minutes

--- app/utils/background-jobs.server.ts
+++ app/utils/background-jobs.server.ts
@@ -160,8 +160,8 @@ export function initializeBackgroundJobs() {
 		() => {
 			runInvoiceDownloadJob().catch(console.error)
 		},
-		15 * 60 * 1000,
-	) // 15 minutes
+		10 * 60 * 1000,
+	) // 10 minutes
 
 	// Schedule the reviews fetch job to run daily
 	jobIntervals.reviewsFetch = setInterval(
